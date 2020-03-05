f8c3260cbaa3f37d638139ca5ea9c2b90331e5eb
mdarafay7 <mdarafay@gmail.com>
2020-03-04 22:56:40 -0600

    working

--- assignment.py
+++ assignment.py
@@ -24,7 +24,14 @@ def normalize(myDict):
     return myDict
 
 
+
+
+
+
+
+
 nltk.download('stopwords')
+#read corpus
 corpusroot = './presidential_debates/presidential_debates'
 debates_store={}
 for filename in os.listdir(corpusroot):
@@ -34,14 +41,14 @@ for filename in os.listdir(corpusroot):
     doc = doc.lower()
     debates_store.update({filename:doc})
 
-
+#tokenize
 debates_tokenized_store={}
 tokenizer = RegexpTokenizer(r'[a-zA-Z]+')
 for debate in debates_store:
     tokens=tokenizer.tokenize(debates_store[debate])
     debates_tokenized_store.update({debate : tokens})
 
-
+#stopwords remove
 debates_stopwords_removed={}
 stop_words = set(stopwords.words('english'))
 for debate in debates_tokenized_store:
@@ -51,7 +58,7 @@ for debate in debates_tokenized_store:
             filtered_debate.append(word)
     debates_stopwords_removed.update({debate : filtered_debate})
 
-
+#stem
 debates_stemmed={}
 stemmer = PorterStemmer()
 for debate in debates_stopwords_removed:
@@ -60,11 +67,10 @@ for debate in debates_stopwords_removed:
         stemmed_debate.append(stemmer.stem(word))
     debates_stemmed.update({debate : stemmed_debate})
 
+#tf-idf
 word_freq_dict={}
-counter=0
 tf_idf_vector_store={}
 document_frequency_vector={}
-#
 for debate in debates_stemmed:
     tf_idf_vector={}
     for word in debates_stemmed[debate]:
@@ -94,10 +100,86 @@ for debate in debates_stemmed:
 
     tf_idf_vector_store.update({debate : normalize(tf_idf_vector)})
 
+# postings list
+postings_list_store={}
+for debate in tf_idf_vector_store:
+    for word in tf_idf_vector_store[debate]:
+        postings_list={}
+        postings_list_sorted={}
+        if tf_idf_vector_store[debate][word] in postings_list_store:
+            continue
+        else:
+            for search_debate in tf_idf_vector_store:
+                if word in tf_idf_vector_store[search_debate]:
+                    postings_list.update({search_debate : tf_idf_vector_store[search_debate][word]})
 
-# print(tf_idf_vector_store)
+                else:
+                    continue
+
+        order=sorted(postings_list, key=postings_list.get, reverse=True)[:10]
+        for val in order:
+            postings_list_sorted.update({val : postings_list[val]})
+
+        postings_list_store.update({word : postings_list_sorted})
+
+#
+# print(postings_list_store[stemmer.stem("clinton")])
+
+
+
+def getweight(document,token):
+    return(tf_idf_vector_store[document][token])
+
+def getidf(token):
+    df=0
+    if token in document_frequency_vector:
+        df=document_frequency_vector.get(token)
+    else:
+        for search_debate in debates_stemmed:
+            if word in debates_stemmed[search_debate]:
+                df+=1
+
+    return(math.log((30/df),10))
+
+def query(query):
+    store={}
+    stemmer = PorterStemmer()
+    tokenizer = RegexpTokenizer(r'[a-zA-Z]+')
+    query = query.lower()
+    query=tokenizer.tokenize(query)
+
+    tf_idf_vector_store={}
+    for token in query:
+        tf_idf=1+math.log(query.count(token),10)
+        tf_idf_vector_store.update({token : tf_idf})
+
+    tf_idf_vector_store=normalize(tf_idf_vector_store)
+
+
+    for document in debates_store:
+        summer=[]
+        for token in query:
+            if stemmer.stem(token) not in postings_list_store:
+                continue
+            if document in postings_list_store[stemmer.stem(token)]:
+                query_vector=tf_idf_vector_store[token]
+                summer.append((query_vector*postings_list_store[stemmer.stem(token)][document]))
+            else:
+                query_vector=tf_idf_vector_store[token]
+                upper_bound=postings_list_store[stemmer.stem(token)][list(postings_list_store[stemmer.stem(token)])[-1]]
+                summer.append(query_vector*(upper_bound))
+        store.update({document : sum(summer)})
+
+    order=sorted(store, key=store.get, reverse=True)[:1]
+
+    return store[order[0]]
+
+
+print(getweight("2012-10-03.txt","health"))
+print(query("health insurance wall street"))
+print(getweight("2012-10-03.txt","health"))
+print(getidf("health"))
+
+
 stop = timeit.default_timer()
 print('Time: ', stop - start)
-
-
-print(tf_idf_vector_store["1976-10-22.txt"][stemmer.stem("agenda")])
